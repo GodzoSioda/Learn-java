0c553f83828a7699ec0e4922dddeb20b59453ffe
GodzoSioda <godzosioda@gmail.com>
2023-04-30 13:41:54 +0300

    The second attempt

--- myNewProject/src/myNewProject/HelloWold.java
+++ myNewProject/src/myNewProject/HelloWold.java
@@ -7,15 +7,13 @@ public class HelloWold {
 	public static void main(String[] args) {
 		
 		String title = "Message";
-		String text = "I will continue to learn Java";
+		String text = "Learn Java";
 		JOptionPane.showMessageDialog(
 				null,
 				text,
 				title,
 				JOptionPane.WARNING_MESSAGE
 				);
-         
-		//System.out.print("Hello World");
 	}
 
 }
