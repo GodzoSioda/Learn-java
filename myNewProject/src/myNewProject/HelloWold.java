600f8728046a16be03f96544f187eee3e47abf22
GodzoSioda <godzosioda@gmail.com>
2023-04-30 12:44:18 +0300

    My first commit from Eclipse

---
+++ myNewProject/src/myNewProject/HelloWold.java
@@ -0,0 +1,21 @@
+package myNewProject;
+
+import javax.swing.*;
+
+public class HelloWold {
+
+	public static void main(String[] args) {
+		
+		String title = "Message";
+		String text = "I will continue to learn Java";
+		JOptionPane.showMessageDialog(
+				null,
+				text,
+				title,
+				JOptionPane.WARNING_MESSAGE
+				);
+         
+		//System.out.print("Hello World");
+	}
+
+}
